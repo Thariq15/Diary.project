b7d328f6dc182020c112cfab897a1a6b99325deb
Thariq15 <thariqaziz1501@gmail.com>
2023-10-25 13:22:01 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# @app.route('/diary', methods=['GET'])
+# def show_diary():
+#     sample_receive = request.args.get('sample_give')
+#     print(sample_receive)
+#     return jsonify({'msg': 'GET request complete!'})
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.mydiary.find({},{'_id':False}))
+    return jsonify({'articles': articles})
+
+# @app.route('/diary', methods=['POST'])
+# def save_diary():
+#     sample_receive = request.form['sample_give']
+#     print(sample_receive)
+#     return jsonify({'msg': 'POST request complete!'})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form["title_give"]
+    content_receive = request.form["content_give"]
+
+    file = request.files["file_give"]
+    
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    extension = file.filename.split('.')[-1]
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+
+    profile = request.files['profile_give']
+
+    extension = profile.filename.split('.')[-1]
+    profilename = f'profile-{mytime}.{extension}'
+    save_to1 = f'static/{profilename}'
+    profile.save(save_to1)
+
+    time = today.strftime('%Y.%m.%d')
+
+    doc = {
+    'file': filename,
+    'profile' : profilename,
+    'title': title_receive,
+    'content': content_receive,
+    'time' : time
+    }
+    db.mydiary.insert_one(doc)
+    return jsonify({'msg':'Upload complete!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
